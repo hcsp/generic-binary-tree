4f389e2e3ea92c954af61e6c96418894549969ac
kungua <52815012+kungua@users.noreply.github.com>
2019-09-25 17:03:21 +0800

    generic binary tree (#2)
    generic binary tree (#2)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,31 +1,61 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class Main {
-    static class IntBinaryTreeNode {
-        int value;
-        IntBinaryTreeNode left;
-        IntBinaryTreeNode right;
-    }
-
-    static class StringBinaryTreeNode {
-        String value;
-        StringBinaryTreeNode left;
-        StringBinaryTreeNode right;
-    }
-
-    static class DoubleBinaryTreeNode {
-        double value;
-        DoubleBinaryTreeNode left;
-        DoubleBinaryTreeNode right;
-    }
-
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其范型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode<T> left;
+        BinaryTreeNode<T> right;
+    }
 
     // 范型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> nodeList = new ArrayList<>();
+        Deque<BinaryTreeNode<T>> stack = new ArrayDeque<>();
+        BinaryTreeNode<T> treeNode = root;
+        while (treeNode != null || !stack.isEmpty()) {
+            while (treeNode != null) {
+                stack.push(treeNode);
+                treeNode = treeNode.left;
+            }
+            if (!stack.isEmpty()) {
+                BinaryTreeNode<T> tailNode = stack.pop();
+                nodeList.add(tailNode.value);
+                treeNode = tailNode.right;
+            }
+        }
+        return nodeList;
+    }
+
+    public static void main(String[] args) {
+        Main.BinaryTreeNode<Integer> node1 = new Main.BinaryTreeNode<>();
+        Main.BinaryTreeNode<Integer> node2 = new Main.BinaryTreeNode<>();
+        Main.BinaryTreeNode<Integer> node3 = new Main.BinaryTreeNode<>();
+        Main.BinaryTreeNode<Integer> node4 = new Main.BinaryTreeNode<>();
+        Main.BinaryTreeNode<Integer> node5 = new Main.BinaryTreeNode<>();
+        Main.BinaryTreeNode<Integer> node6 = new Main.BinaryTreeNode<>();
+
+        {
+            node1.value = 1;
+            node2.value = 2;
+            node3.value = 3;
+            node4.value = 4;
+            node5.value = 5;
+            node6.value = 6;
+
+            node1.left = node2;
+            node1.right = node3;
+
+            node2.left = node4;
+            node2.right = node5;
+
+            node3.right = node6;
+        }
+        inorderTraversal(node1);
     }
 }
