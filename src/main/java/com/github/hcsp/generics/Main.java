989f64c941928e1d7f2fd3764dbc12afe953cbd3
Yuyan <845058547@qq.com>
2019-10-01 14:49:21 +0800

    Generic comply binaryTree

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,5 +1,8 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
 
 public class Main {
@@ -22,10 +25,30 @@ public class Main {
     }
 
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其范型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+    }
 
     // 范型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> list = new ArrayList<>();
+        Deque<BinaryTreeNode<T>> deque = new ArrayDeque<>();
+        BinaryTreeNode node = root;
+        if (root == null) {
+            return list;
+        }
+        while ((node != null) || (!deque.isEmpty())) {
+            if (node != null) {
+                deque.add(node);
+                node = node.left;
+            } else {
+                node = deque.removeLast();
+                list.add((T) node.value);
+                node = node.right;
+            }
+        }
+        return list;
     }
 }
