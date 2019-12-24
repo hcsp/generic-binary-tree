056a323d7c78f540162d85bb6a2481d22dafc734
shenwl <shenweilun96@gmail.com>
2019-12-24 22:40:47 +0800

    范型化的二叉树遍历算法

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,31 +1,37 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
-    static class IntBinaryTreeNode {
-        int value;
-        IntBinaryTreeNode left;
-        IntBinaryTreeNode right;
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode<T> left;
+        BinaryTreeNode<T> right;
     }
 
-    static class StringBinaryTreeNode {
-        String value;
-        StringBinaryTreeNode left;
-        StringBinaryTreeNode right;
-    }
+    public static <T> void traverseNode(BinaryTreeNode<T> root, List<T> result) {
+        Stack<BinaryTreeNode<T>> nodeStack = new Stack<>();
+        BinaryTreeNode<T> p = root;
 
-    static class DoubleBinaryTreeNode {
-        double value;
-        DoubleBinaryTreeNode left;
-        DoubleBinaryTreeNode right;
-    }
+        while (p != null || !nodeStack.isEmpty()) {
+            while (p != null) {
+                nodeStack.push(p);
+                p = p.left;
+            }
+            BinaryTreeNode<T> cur = nodeStack.pop();
+            result.add(cur.value);
+            p = cur.right;
+        }
 
-    // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其范型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    }
 
     // 范型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> result = new ArrayList<>();
+
+        traverseNode(root, result);
+        return result;
     }
 }
