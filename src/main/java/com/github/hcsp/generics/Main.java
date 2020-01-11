10e9491ee79fc96c74b7cfb4311400dd6a547e94
恶魔妹妹买面膜 <51756846+chelegen@users.noreply.github.com>
2020-01-11 10:59:42 +0800

    泛型二叉树 (#26)
    泛型二叉树 (#26)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,31 +1,33 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    static class IntBinaryTreeNode {
-        int value;
-        IntBinaryTreeNode left;
-        IntBinaryTreeNode right;
-    }
-
-    static class StringBinaryTreeNode {
-        String value;
-        StringBinaryTreeNode left;
-        StringBinaryTreeNode right;
-    }
-
-    static class DoubleBinaryTreeNode {
-        double value;
-        DoubleBinaryTreeNode left;
-        DoubleBinaryTreeNode right;
-    }
-
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其范型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode<T> left;
+        BinaryTreeNode<T> right;
+    }
 
     // 范型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> list = new ArrayList<>();
+        eachBinaryTreeNode(list, root);
+        return list;
+    }
+
+    private static <T> void eachBinaryTreeNode(List<T> list, BinaryTreeNode<T> root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null) {
+            eachBinaryTreeNode(list, root.left);
+        }
+        list.add(root.value);
+        if (root.right != null) {
+            eachBinaryTreeNode(list, root.right);
+        }
     }
 }
