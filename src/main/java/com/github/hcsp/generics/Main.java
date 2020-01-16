49a3a3d5889d6fd605bd394d086784c5153e40cd
tongkang <actongkang@outlook.com>
2020-01-16 14:38:31 +0800

    范型化的二叉树遍历算法 (#27)
    范型化的二叉树遍历算法 (#27)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,5 +1,6 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -22,10 +23,23 @@ public class Main {
     }
 
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其范型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode<T> left;
+        BinaryTreeNode<T> right;
+
+    }
 
     // 范型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> result = new ArrayList<>();
+        if (root.left != null) {
+            result.addAll(inorderTraversal(root.left));
+        }
+        result.add(root.value);
+        if (root.right != null) {
+            result.addAll(inorderTraversal(root.right));
+        }
+        return result;
     }
 }
