a7a6f73d7876edeb44192fcb9b23037d9cf9f346
PingAn_NetBar <840495823@qq.com>
2020-02-14 23:40:20 +0800

    泛型抵御需求带来的变化 (#37)
    泛型抵御需求带来的变化 (#37)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
     static class IntBinaryTreeNode {
@@ -22,10 +24,33 @@ public class Main {
     }
 
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+    }
 
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+
+        List<T> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<T> stack = new Stack<>();
+        do {
+            while (root != null) {
+                stack.push((T) root);
+                root = root.left;
+            }
+            if (!stack.isEmpty()) {
+                root = (BinaryTreeNode<T>) stack.pop();
+                result.add(root.value);
+                root = root.right;
+            }
+        } while (!stack.isEmpty() || root != null);
+        return result;
     }
+
 }
+
