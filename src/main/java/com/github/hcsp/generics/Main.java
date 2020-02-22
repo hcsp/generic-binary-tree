4cf7ed00ae874d4d11bd9c4d1f47899879e49275
ws1603 <18388866683@163.com>
2020-02-22 12:46:02 +0800

    泛型化的二叉树

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
     static class IntBinaryTreeNode {
@@ -22,10 +24,35 @@ public class Main {
     }
 
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+    }
+
 
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> list = new ArrayList<>();
+        Stack<BinaryTreeNode<T>> stack = new Stack<>();
+        if (root != null) {
+            BinaryTreeNode<T> node = root;
+            stack.push(node);
+            node = node.left;
+            while (node != null || !stack.isEmpty()) {
+                while (node != null) {
+                    stack.push(node);
+                    node = node.left;
+                }
+                node = stack.pop();
+                list.add(node.value);
+                if (node.right != null) {
+                    node = node.right;
+                } else {
+                    node = null;
+                }
+            }
+        }
+        return list;
     }
 }
