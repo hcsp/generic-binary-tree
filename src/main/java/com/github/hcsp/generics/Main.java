8ac5dd49f6ab5d9b5ed0c26821466484273bdb94
PudgeWL <nbhaohao@gmail.com>
2020-06-03 21:07:05 +0800

    完成挑战 (#68)
    完成挑战 (#68)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,31 +1,30 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    static class IntBinaryTreeNode {
-        int value;
-        IntBinaryTreeNode left;
-        IntBinaryTreeNode right;
-    }
-
-    static class StringBinaryTreeNode {
-        String value;
-        StringBinaryTreeNode left;
-        StringBinaryTreeNode right;
-    }
-
-    static class DoubleBinaryTreeNode {
-        double value;
-        DoubleBinaryTreeNode left;
-        DoubleBinaryTreeNode right;
-    }
-
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode<T> left;
+        BinaryTreeNode<T> right;
+    }
+
+    public static <T> List<T> midOrder(BinaryTreeNode<T> root, List<T> list) {
+        if (root.left != null) {
+            midOrder(root.left, list);
+        }
+        list.add(root.value);
+        if (root.right != null) {
+            midOrder(root.right, list);
+        }
+        return list;
+    }
 
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> list = new ArrayList<>();
+        return midOrder(root, list);
     }
 }
