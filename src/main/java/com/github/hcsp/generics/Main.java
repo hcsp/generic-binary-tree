340674b3ef089d309a899d74fcdc551b30d94a61
BenHao Huang <33121651+Lazzben@users.noreply.github.com>
2020-06-29 22:56:26 +0800

    增加递归算法 (#75)
    增加递归算法 (#75)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,5 +1,6 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -22,10 +23,45 @@ public class Main {
     }
 
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode<T> left;
+        BinaryTreeNode<T> right;
+    }
 
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
+
+    //非递归
+//    public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
+//        Stack<BinaryTreeNode<T>> stack = new Stack<>();
+//        ArrayList<T> result = new ArrayList<>();
+//        BinaryTreeNode<T> node = root;
+//        do {
+//            while (node != null) {
+//                stack.push(node);
+//                node = node.left;
+//            }
+//            node = stack.pop();
+//            result.add(node.value);
+//            node = node.right;
+//        } while (!stack.isEmpty() || node != null);
+//        return result;
+//    }
+
+    //递归
+    public static <T> void inorderTraversalRecursion(BinaryTreeNode<T> node, List<T> result) {
+        if (node.left != null) {
+            inorderTraversalRecursion(node.left, result);
+        }
+        result.add(node.value);
+        if (node.right != null) {
+            inorderTraversalRecursion(node.right, result);
+        }
+    }
+
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        ArrayList<T> result = new ArrayList<>();
+        inorderTraversalRecursion(root, result);
+        return result;
     }
 }
