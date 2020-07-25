602be71cc5f6c3b3ee778010435e08743b29528c
McDul <McDul19960103@gmail.com>
2020-07-25 11:54:40 +0800

    泛型化的二叉树中序遍历

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,31 +1,48 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    static class IntBinaryTreeNode {
-        int value;
-        IntBinaryTreeNode left;
-        IntBinaryTreeNode right;
-    }
-
-    static class StringBinaryTreeNode {
-        String value;
-        StringBinaryTreeNode left;
-        StringBinaryTreeNode right;
-    }
-
-    static class DoubleBinaryTreeNode {
-        double value;
-        DoubleBinaryTreeNode left;
-        DoubleBinaryTreeNode right;
-    }
 
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode<T> left;
+        BinaryTreeNode<T> right;
+    }
 
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
+    // 递归中序遍历：
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> resultList = new ArrayList<>();
+        BinaryTreeNode<T> node = root;
+        reTraversal(root, resultList);
+        return resultList;
     }
+
+    private static <T> void reTraversal(BinaryTreeNode<T> root, List<T> resultList) {
+        if (root != null) {
+            reTraversal(root.left, resultList);
+            resultList.add(root.value);
+            reTraversal(root.right, resultList);
+        }
+    }
+
+    // 非递归中序遍历：
+//    public static <T> List<T> inorderTraversal2(BinaryTreeNode<T> root) {
+//        List<T> resultList = new ArrayList<>();
+//        Stack<BinaryTreeNode<T>> stack = new Stack<>();
+//        BinaryTreeNode<T> tempRoot = root;
+//        while (tempRoot != null || !stack.isEmpty()) {
+//            while (tempRoot != null) {
+//                stack.push(tempRoot);
+//                tempRoot = tempRoot.left;
+//            }
+//            tempRoot = stack.pop();
+//            resultList.add(tempRoot.value);
+//            tempRoot = tempRoot.right;
+//        }
+//        return resultList;
+//    }
 }
