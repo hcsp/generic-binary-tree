e1ec7afdbd8185fdb148ecf6b32a04c4bf8efc35
Ryan <49991585+Ryan-Y-C@users.noreply.github.com>
2020-08-31 18:36:35 +0800

    对二叉树进行中序遍历。 (#87)
    对二叉树进行中序遍历。 (#87)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
     static class IntBinaryTreeNode {
@@ -22,10 +24,31 @@ public class Main {
     }
 
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode left;
+        BinaryTreeNode right;
+    }
 
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> list = new ArrayList<>();
+        if (root == null) {
+            return null;
+        }
+        Stack<BinaryTreeNode<T>> stack = new Stack<>();
+        BinaryTreeNode current = root;
+
+        while (current != null || !stack.empty()) {
+            while (current != null) {
+                stack.push(current);
+                current = current.left;
+            }
+            BinaryTreeNode<T> node = stack.pop();
+            list.add(node.value);
+            current = node.right;
+        }
+        return list;
     }
+
 }
