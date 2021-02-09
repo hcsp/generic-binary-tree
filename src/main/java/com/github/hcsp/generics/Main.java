4374e222e2e6b41a9ec3e0e8ad2204d3060483d1
xu-kai-quan <50357378+xu-kai-quan@users.noreply.github.com>
2021-02-09 09:46:51 +0800

    泛型化的二叉树遍历算法 (#101)
    泛型化的二叉树遍历算法 (#101)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,5 +1,6 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -22,10 +23,30 @@ public class Main {
     }
 
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
-    static class BinaryTreeNode<T> {}
+
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode<T> left;
+        BinaryTreeNode<T> right;
+    }
 
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> list = new ArrayList<>();
+        BinaryTreeNode<T> current = root;
+        midOrder(list, current);
+        return list;
+    }
+
+    private static <T> void midOrder(List<T> list, BinaryTreeNode<T> root) {
+        if (root.left != null) {
+            midOrder(list, root.left);
+        }
+
+        list.add(root.value);
+
+        if (root.right != null) {
+            midOrder(list, root.right);
+        }
     }
 }
