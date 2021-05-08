731199f6abbe8c78bba24036d7d4f5551c466b28
FengZhou <49605925+greek-zzf@users.noreply.github.com>
2021-05-08 09:01:39 +0800

    finish generic-binaty-tree (#103)
    finish generic-binaty-tree (#103)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,5 +1,6 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -22,10 +23,26 @@ public class Main {
     }
 
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode<T> left;
+        BinaryTreeNode<T> right;
+    }
+
 
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> values = new ArrayList<>();
+
+        if (root.left != null) {
+            values.addAll(inorderTraversal(root.left));
+        }
+
+        values.add(root.value);
+
+        if (root.right != null) {
+            values.addAll(inorderTraversal(root.right));
+        }
+        return values;
     }
 }
