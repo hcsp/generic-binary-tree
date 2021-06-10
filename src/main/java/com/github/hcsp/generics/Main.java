ec21fd6308bf7accf21f048a6c8031f2867e1a72
giantpetter <65336996+giantpetter@users.noreply.github.com>
2021-06-10 17:18:28 +0800

    递归实现二叉树中序遍历

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,5 +1,6 @@
 package com.github.hcsp.generics;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -22,10 +23,24 @@ public class Main {
     }
 
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
-    static class BinaryTreeNode<T> {}
+    static class BinaryTreeNode<T> {
+        T value;
+        BinaryTreeNode<T> left;
+        BinaryTreeNode<T> right;
+    }
 
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        return null;
+        List<T> result = new ArrayList<>();
+        recurseInorderTraversal(result, root);
+        return result;
+    }
+
+    private static <T> void recurseInorderTraversal(List<T> result, BinaryTreeNode<T> root) {
+        if (root != null) {
+            recurseInorderTraversal(result, root.left);
+            result.add(root.value);
+            recurseInorderTraversal(result, root.right);
+        }
     }
 }
