52345dc37dbf3d8dad1de29ba1134f599690a6ca
Jerry Wang <1976883731@qq.com>
2021-11-19 01:20:59 +0800

    Generic binary tree test

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -4,32 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    static class IntBinaryTreeNode {
-        int value;
-        IntBinaryTreeNode left;
-        IntBinaryTreeNode right;
-    }
-
-    static class StringBinaryTreeNode {
-        String value;
-        StringBinaryTreeNode left;
-        StringBinaryTreeNode right;
-    }
-
-    static class DoubleBinaryTreeNode {
-        double value;
-        DoubleBinaryTreeNode left;
-        DoubleBinaryTreeNode right;
-    }
-
-    // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
     static class BinaryTreeNode<T> {
         T value;
         BinaryTreeNode<T> left;
         BinaryTreeNode<T> right;
     }
 
-    // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
         List<T> list = new ArrayList<>();
         if (root.left != null) {
