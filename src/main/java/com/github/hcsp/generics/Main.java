a0d60cc2d91410b44dd600c917cd0ac818020bce
codingories <34835258+codingories@users.noreply.github.com>
2021-12-02 10:51:31 +0800

    finish (#112)
    finish (#112)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -2,6 +2,7 @@ package com.github.hcsp.generics;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
     static class IntBinaryTreeNode {
@@ -32,12 +33,19 @@ public class Main {
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
         List<T> list = new ArrayList<>();
-        if (root.left != null) {
-            list.addAll(inorderTraversal(root.left));
-        }
-        list.add(root.value);
-        if (root.right != null) {
-            list.addAll(inorderTraversal(root.right));
+        Stack<BinaryTreeNode<T>> treeNodeStack = new Stack<>();
+        BinaryTreeNode node = root;
+
+        while (node != null || !treeNodeStack.isEmpty()) {
+            while (node != null) {
+                treeNodeStack.push(node);
+                node = node.left;
+            }
+            if (!treeNodeStack.isEmpty()) {
+                BinaryTreeNode<T> noleftnode = treeNodeStack.pop();
+                list.add(noleftnode.value);
+                node = noleftnode.right;
+            }
         }
         return list;
     }
