bea23d74b6a1fb7d6d827f535312e0440afb9282
hedongsheng <he77505357@163.com>
2022-01-25 11:58:10 +0800

    泛型化的二叉树遍历算法(中序) (#115)
    泛型化的二叉树遍历算法(中序) (#115)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,28 +1,11 @@
 package com.github.hcsp.generics;
 
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Main {
-    static class IntBinaryTreeNode {
-        int value;
-        IntBinaryTreeNode left;
-        IntBinaryTreeNode right;
-    }
-
-    static class StringBinaryTreeNode {
-        String value;
-        StringBinaryTreeNode left;
-        StringBinaryTreeNode right;
-    }
-
-    static class DoubleBinaryTreeNode {
-        double value;
-        DoubleBinaryTreeNode left;
-        DoubleBinaryTreeNode right;
-    }
-
-    // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
     static class BinaryTreeNode<T> {
         T value;
         BinaryTreeNode<T> left;
@@ -32,13 +15,21 @@ public class Main {
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
         List<T> list = new ArrayList<>();
-        if (root.left != null) {
-            list.addAll(inorderTraversal(root.left));
+        Stack<BinaryTreeNode<T>> binaryTreeStack = new Stack<>();
+        BinaryTreeNode<T> treeNode = root;
+        while (treeNode != null || !binaryTreeStack.isEmpty()) {
+            while (treeNode != null) {
+                binaryTreeStack.push(treeNode);
+                treeNode = treeNode.left;
+            }
+            if (!binaryTreeStack.isEmpty()) {
+                treeNode = binaryTreeStack.pop();
+                list.add(treeNode.value);
+                treeNode = treeNode.right;
+            }
         }
-        list.add(root.value);
-        if (root.right != null) {
-            list.addAll(inorderTraversal(root.right));
-        }
+
+
         return list;
     }
 }
