9f4576ba40caba63a051ac8cf85b377c3b4c2a6d
wjxalexander <37684072+wjxalexander@users.noreply.github.com>
2023-05-10 22:24:32 +0800

    done (#123)
    done (#123)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -4,24 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    static class IntBinaryTreeNode {
-        int value;
-        IntBinaryTreeNode left;
-        IntBinaryTreeNode right;
-    }
-
-    static class StringBinaryTreeNode {
-        String value;
-        StringBinaryTreeNode left;
-        StringBinaryTreeNode right;
-    }
-
-    static class DoubleBinaryTreeNode {
-        double value;
-        DoubleBinaryTreeNode left;
-        DoubleBinaryTreeNode right;
-    }
-
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
     static class BinaryTreeNode<T> {
         T value;
@@ -32,13 +14,16 @@ public class Main {
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
         List<T> list = new ArrayList<>();
-        if (root.left != null) {
-            list.addAll(inorderTraversal(root.left));
-        }
-        list.add(root.value);
-        if (root.right != null) {
-            list.addAll(inorderTraversal(root.right));
-        }
+        traversalHelper(root, list);
         return list;
+    }
+
+    public static <T> void traversalHelper(BinaryTreeNode<T> root, List<T> list) {
+        if (root == null) {
+            return;
+        }
+        traversalHelper(root.left, list);
+        list.add(root.value);
+        traversalHelper(root.right, list);
     }
 }
