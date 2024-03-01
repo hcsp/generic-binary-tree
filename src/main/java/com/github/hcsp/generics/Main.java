46a71ec4d918c1845394492ffad9a99e87030d9c
rf4github <39894049+rf818@users.noreply.github.com>
2024-03-01 11:48:07 +0800

    采用了非递归的方式进行二叉树中序遍历 (#124)
    采用了非递归的方式进行二叉树中序遍历 (#124)

--- src/main/java/com/github/hcsp/generics/Main.java
+++ src/main/java/com/github/hcsp/generics/Main.java
@@ -1,26 +1,8 @@
 package com.github.hcsp.generics;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
-    static class IntBinaryTreeNode {
-        int value;
-        IntBinaryTreeNode left;
-        IntBinaryTreeNode right;
-    }
-
-    static class StringBinaryTreeNode {
-        String value;
-        StringBinaryTreeNode left;
-        StringBinaryTreeNode right;
-    }
-
-    static class DoubleBinaryTreeNode {
-        double value;
-        DoubleBinaryTreeNode left;
-        DoubleBinaryTreeNode right;
-    }
 
     // 你看，上面三种"二叉树节点"结构相似，内容重复，请将其泛型化，以节省代码
     static class BinaryTreeNode<T> {
@@ -31,14 +13,22 @@ public class Main {
 
     // 泛型化之后，请再编写一个算法，对二叉树进行中序遍历，返回中序遍历的结果
     public static <T> List<T> inorderTraversal(BinaryTreeNode<T> root) {
-        List<T> list = new ArrayList<>();
-        if (root.left != null) {
-            list.addAll(inorderTraversal(root.left));
+        //非递归形式
+        List<T> result = new ArrayList<>();
+        if (root == null) {
+            return result;
         }
-        list.add(root.value);
-        if (root.right != null) {
-            list.addAll(inorderTraversal(root.right));
+        Deque<BinaryTreeNode<T>> stack = new ArrayDeque<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            result.add(root.value);
+            root = root.right;
         }
-        return list;
+        return result;
     }
+
 }
